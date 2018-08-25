091c4ad0af41a0c3cb67693e7dca46ef37959b07
Connor Austin <cjaustin42@gmail.com>
2018-08-25 11:42:24 -0500

    unregistering with library

--- src/index.js
+++ src/index.js
@@ -2,13 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import unregister from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-// registerServiceWorker();
-
-navigator.serviceWorker.getRegistrations().then(function(registrations) {
-for(let registration of registrations) {
-    registration.unregister()
-} })
+unregister();
